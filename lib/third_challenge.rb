9f01eb64e434f6fdc95a95edf28e9e6adf8c5a4e
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-14 08:30:35 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,19 +10,24 @@ def third_challenge
         :age => "54"
         },
       :hero => {
-        :name =>
+        :name => "Romeo",
+        :age => "15",
+        :status => "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Capulet",
+        :age => "50"
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lady Capulet",
+        :age => "51"
         },
       :heroine => {
-        #your key/value pairs here
+        :name => "Juliet",
+        :age => "15"
         },
       :heroine_friends => []
    }
